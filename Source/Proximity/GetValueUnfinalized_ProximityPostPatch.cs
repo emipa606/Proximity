30676ae85ac14baabded9435ff2c6ee3f41327e0
Taranchuk <nfhfyxer@gmail.com>
2023-06-21 20:03:54 +0300

    transpiler bug fix

--- Source/Proximity/GetValueUnfinalized_ProximityPostPatch.cs
+++ Source/Proximity/GetValueUnfinalized_ProximityPostPatch.cs
@@ -63,17 +63,19 @@ public static class StatWorker_GetValueUnfinalized_Patch
         var codes = codeInstructions.ToList();
         var statField = AccessTools.Field(typeof(StatWorker), "stat");
         var setOffsets = AccessTools.Method(typeof(StatWorker_GetValueUnfinalized_Patch), "SetOffsets");
+        var patched = false;
         for (var i = 0; i < codes.Count; i++)
         {
             var code = codes[i];
             yield return code;
-            if (i > 1 && codes[i].opcode == OpCodes.Brfalse && codes[i - 1].opcode == OpCodes.Ldloc_1)
+            if (!patched && i > 1 && codes[i].opcode == OpCodes.Brfalse && codes[i - 1].opcode == OpCodes.Ldloc_1)
             {
                 yield return new CodeInstruction(OpCodes.Ldloca_S, 0);
                 yield return new CodeInstruction(OpCodes.Ldarg_0);
                 yield return new CodeInstruction(OpCodes.Ldfld, statField);
                 yield return new CodeInstruction(OpCodes.Ldarg_S, 1);
                 yield return new CodeInstruction(OpCodes.Call, setOffsets);
+                patched = true;
             }
         }
     }
